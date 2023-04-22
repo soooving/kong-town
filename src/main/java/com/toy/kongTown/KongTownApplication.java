39f3ab374a09b69719f93750dbd1e1efcf906f72
soooving <soooving@gmail.com>
2023-04-23 02:32:25 +0900

    init: 초기 세팅

---
+++ src/main/java/com/toy/kongTown/KongTownApplication.java
@@ -0,0 +1,13 @@
+package com.toy.kongTown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KongTownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KongTownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toy/kongTown/KongTownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.kongTown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KongTownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
