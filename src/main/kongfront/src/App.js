dc932003df4c6475fa4abb94f427204e771e4294
soooving <soooving@gmail.com>
2023-04-27 17:25:47 +0900

    feat:

--- src/main/kongfront/src/App.js
+++ src/main/kongfront/src/App.js
@@ -1,20 +1,13 @@
 // src/main/kongfront/src/App.js
 
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import React from 'react';
 
 function App() {
-  const [hello, setHello] = useState('')
 
-  useEffect(() => {
-    axios.get('/api/hello')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-  }, []);
 
   return (
       <div>
-        백엔드에서 가져온 데이터입니다 : {hello}
+
       </div>
   );
 }
