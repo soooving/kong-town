07d3491de83d9472e502003cd3dd943d68ea64cd
soooving <soooving@gmail.com>
2023-05-02 17:59:40 +0900

    feat: Login.js 퍼블 작업중

--- src/main/kongfront/src/app/App.js
+++ src/main/kongfront/src/app/App.js
@@ -1,11 +1,18 @@
 import { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Login from '../common/Login'
 
 
 class App extends Component {
 
     render(){
         return (
-            <h2>aa</h2>
+            <>
+                <Routes>
+                    <Route exact path="/login" element={ <Login /> }></Route>
+                </Routes>
+            </>
         );
     }
 }
---
+++ src/main/kongfront/src/common/Login.js
@@ -0,0 +1,49 @@
+import { Component } from 'react';
+
+/* ? CSS */
+import '../assets/css/reset.css'
+import '../assets/css/main.css'
+import '../assets/css/common.css'
+
+/* ? IMAGE */
+import logo from '../assets/image/logo-bean.png';
+
+
+class Login extends Component {
+
+    render(){
+        return (
+            <div className={'login'}>
+                <div className={'login-logo'}>
+                    <img className={'img-logo lg'} src={ logo }  alt={'로고'} />
+                </div>
+                <div className={'login-area'}>
+                    <div className={'login-area-input'}>
+                        <ul>
+                            <li>아이디</li>
+                            <li>
+                                <input type={'text'} />
+                            </li>
+                        </ul>
+                        <ul>
+                            <li>패스워드</li>
+                            <li>
+                                <input type={'text'} />
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={'login-area-btn'}>
+                        <a className={'a-btn main-btn'} href={'/'}>로그인</a>
+                    </div>
+                    <div className={'login-area-link'}>
+                        <a className={'a-text-btn'} href={'/'}>회원가입</a>
+                        <a className={'a-text-btn'} href={'/'}>아이디찾기</a>
+                        <a className={'a-text-btn'} href={'/'}>비밀번호변경</a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
