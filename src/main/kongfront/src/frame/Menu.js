ac4ec20a86fd567e1acf39e700abf35700a409ba
soooving <soooving@gmail.com>
2023-05-04 17:59:41 +0900

    feat: layout css작업 중간커밋

--- src/main/kongfront/src/frame/Menu.js
+++ src/main/kongfront/src/frame/Menu.js
@@ -1,5 +1,12 @@
+/* ? IMAGE */
+import logo from '../assets/image/logo-bean.png';
+
 const Menu = () => (
     <div className={'menu'}>
+        <div className={'menu-logo'}>
+            <img className={'img-logo lg'} src={ logo }  alt={'로고'} />
+        </div>
+
         hi menu
     </div>
 );
